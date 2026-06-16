61d091a60af5a81b69d70947514e0284b7867904
Abhinav Rag <abhi.rag0512@gmail.com>
2026-06-16 14:07:00 +0530

    Updating CORS

--- backend/main.py
+++ backend/main.py
@@ -5,10 +5,7 @@ from router.resume import router as resume_router
 app = FastAPI(title="Resume Analyzer API", version="1.0.0")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:5174",
-    ],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"]
 )
